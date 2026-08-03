135d3ff137bda2ccafb44edc10b472b96d9cb220
ahmedelkadii <ahmedelkadii@kodeflowstudios.com>
2026-08-04 01:56:43 +0300

    Completes [TUI-5]: Project data

---
+++ src/config.rs
@@ -0,0 +1,2 @@
+pub const LANG: &str = "ENG";
+pub const VERSION: &str = "v1.0.0";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![allow(warnings)]
+mod config;
+// use crate::config::*;
+
+use chrono::{DateTime, Utc};
+
+struct ProjectData {
+    git_tracked: bool,
+    name: String,
+    path: String,
+    editor_version: String,
+    last_opened: DateTime<Utc>
+}
+
+impl ProjectData {
+    fn create_project() -> ProjectData {
+        return ProjectData { 
+            git_tracked: false,
+            name: String::from("DEFAULT"),
+            path: String::from("HOME_DIR"),
+            editor_version: String::from("EDITOR_VERSION"),
+            last_opened: Utc::now()
+        };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut p1: ProjectData = ProjectData::create_project();
+
+    println!("Name: {}", p1.name);
 }
